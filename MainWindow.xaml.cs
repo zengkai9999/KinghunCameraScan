db394298994817254ecb6655a4bf85a64ec0b484
zeng <zengkai9999@126.com>
2020-09-02 14:11:12 +0800

    800万插值1200万改1300万；改图标名称；

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1647,7 +1647,7 @@ namespace CameraScan
                 global.pHostCamera.PreHeight = 2448;
             }
 
-            if (openWidth == 4000 && openHeight == 3000)
+            if (openWidth == 4208 && openHeight == 3120)
             {
                 global.pHostCamera.PreWidth = 3264;
                 global.pHostCamera.PreHeight = 2448;
