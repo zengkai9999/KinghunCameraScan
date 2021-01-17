ae65a5ad250546551657d9d45f210c9617858615
zeng <zengkai9999@126.com>
2021-01-17 14:18:52 +0800

    修改法文上一页下一页没有翻译到；修改自定义命名窗口没有翻译到；

--- FixedNameDlg.xaml.cs
+++ FixedNameDlg.xaml.cs
@@ -79,6 +79,39 @@ namespace CameraScan
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            ResourceDictionary langRd = null;
+            try
+            {
+                if (global.pLangusge == 0)
+                    langRd = System.Windows.Application.LoadComponent(new Uri(@"zh-cn.xaml", UriKind.Relative)) as ResourceDictionary;
+                if (global.pLangusge == 1)
+                    langRd = System.Windows.Application.LoadComponent(new Uri(@"zh-tw.xaml", UriKind.Relative)) as ResourceDictionary;
+                if (global.pLangusge == 2)
+                    langRd = System.Windows.Application.LoadComponent(new Uri(@"en-us.xaml", UriKind.Relative)) as ResourceDictionary;
+                if (global.pLangusge == 3)
+                    langRd = System.Windows.Application.LoadComponent(new Uri(@"zh-spain.xaml", UriKind.Relative)) as ResourceDictionary;
+                if (global.pLangusge == 4)
+                    langRd = System.Windows.Application.LoadComponent(new Uri(@"zh-Japan.xaml", UriKind.Relative)) as ResourceDictionary;
+                if (global.pLangusge == 5)
+                    langRd = System.Windows.Application.LoadComponent(new Uri(@"zh-Italian.xaml", UriKind.Relative)) as ResourceDictionary;
+                if (global.pLangusge == 6)
+                    langRd = System.Windows.Application.LoadComponent(new Uri(@"zh-French.xaml", UriKind.Relative)) as ResourceDictionary;
+                if (global.pLangusge == 7)
+                    langRd = System.Windows.Application.LoadComponent(new Uri(@"zh-Germany.xaml", UriKind.Relative)) as ResourceDictionary;
+            }
+            catch (Exception e2)
+            {
+                //MessageBox.Show(e2.Message);
+            }
+            if (langRd != null)
+            {
+                if (this.Resources.MergedDictionaries.Count > 0)
+                {
+                    this.Resources.MergedDictionaries.Clear();
+                }
+                this.Resources.MergedDictionaries.Add(langRd);
+            }
+
             global.pFixedNameStr = "";
             this.Focus();
         }
